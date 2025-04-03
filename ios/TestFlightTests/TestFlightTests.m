3a0db575caea86fac9e96e5eef8f041dd337576d
RaulMorales404 <r20160106@gmail.com>
2025-04-03 02:22:24 -0600

    base proyect

--- ios/TestFlightTests/TestFlightTests.m
+++ ios/TestFlightTests/TestFlightTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TodoAppListTests : XCTestCase
+@interface TestFlightTests : XCTestCase
 
 @end
 
-@implementation TodoAppListTests
+@implementation TestFlightTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
